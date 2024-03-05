4d4cecacbda3e9741a15162680c81bd3633003d2
ljx011011 <ljx011011@idoocn.com>
2024-03-05 10:43:25 +0800

    自定义表盘适配思澈平台

---
+++ Demo/src/main/java/test/com/ido/model/CwdDialConfigBean.kt
@@ -0,0 +1,18 @@
+package test.com.ido.model
+
+/**
+ * @author WYong
+ * @date 2023/11/15
+ * @description:
+ */
+data class CwdDialConfigBean(
+        val project: Int,
+        val version: Int,
+        val name: String,
+        val id: Int,
+        val dial_data: MutableList<DialDataBean>
+)
+
+data class DialDataBean(val user: Int, val wtype: Int, val xdata: Int, val dtype: Int, var x: Int, var y: Int, val file: String, val font_size: Int, var r: Int,
+                        var g: Int, var b: Int)
+
